d3ba232dfe0616187efdf69809040c92e24c39f6
karthik-0123 <jupallikarthik04@gmail.com>
2026-07-22 21:41:19 +0530

    Delete tokens.h

--- tokens.h
+++
@@ -1,7 +0,0 @@
-#ifndef TOKENS_H
-#define TOKENS_H
-#include <stdio.h>
-/* To print the token on ouptut file */
-void printToken(FILE *outFptr, char *lexeme, char *tokenType, int line, int pos);
-
-#endif
