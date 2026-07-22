7e177ddd7c767278b20bf8204c8714a0c7ad60d5
karthik-0123 <jupallikarthik04@gmail.com>
2026-07-22 21:41:06 +0530

    Delete tokens.c

--- tokens.c
+++
@@ -1,7 +0,0 @@
-#include "tokens.h"
-#include <stdio.h>
-
-void printToken(FILE *outFptr, char *lexeme, char *tokenType, int line, int pos)
-{
-    fprintf(outFptr, "input.c:%2d:%2d: %-15s %s\n", line, pos, tokenType, lexeme);
-}
